44bb404aaa23d82de3baf54a707cc850136a796f
John Payne <jjrpayne@uvic.ca>
2020-05-07 16:07:18 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function createMap(data){
+	console.log(data);
+	var poi = {lat: data['latitude'], lng: data['longitude']}
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 12,
+		center: poi
+	});
+
+	var marker = new google.maps.Marker({
+		position: poi,
+		map: map
+	});
+}
+
+function initMap() {
+	fetch('http://jj-poi-api.herokuapp.com/poi')
+		.then(result => result.json())
+		.then(data => createMap(data));
+}
+
